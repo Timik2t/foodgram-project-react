0619b1a3299184715b4898f34c0be9d092c944cb
Timik2t <timik-95@yandex.ru>
2022-10-20 02:02:24 +0300

    filters update

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -26,7 +26,7 @@ class AuthorTagFilter(FilterSet):
 
     def filter_is_in_shopping_cart(self, queryset, name, value):
         if value and not self.request.user.is_anonymous:
-            return queryset.filter(cart__user=self.request.user)
+            return queryset.filter(shopping_carts__user=self.request.user)
         return queryset
 
     class Meta:
